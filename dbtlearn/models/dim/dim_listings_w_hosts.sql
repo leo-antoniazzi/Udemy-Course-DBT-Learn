59af2e3cf44f97b37725baa18c30b7aebff5f857
leo-antoniazzi <leo.antoniazzi@icloud.com>
2024-11-25 20:23:49 +0000

    Initial commit with Udemy course files

---
+++ dbtlearn/models/dim/dim_listings_w_hosts.sql
@@ -0,0 +1,29 @@
+WITH
+
+listings AS (
+    SELECT
+    *
+    FROM {{ref('dim_listings_cleansed')}}
+)
+
+, hosts AS (
+    SELECT
+    *
+    FROM {{ref('dim_hosts_cleansed')}}
+)
+
+SELECT
+    l.listing_id,
+    l.listing_name,
+    l.room_type,
+    l.minimum_nights,
+    l.price,
+    l.host_id,
+    h.host_name,
+    h.is_superhost AS host_is_superhost,
+    l.created_at,
+    GREATEST(l.updated_at,h.updated_at) AS updated_at
+
+FROM listings AS l  
+LEFT JOIN hosts AS h 
+    ON l.host_id = l.host_id
